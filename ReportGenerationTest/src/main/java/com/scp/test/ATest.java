3e118a28fcf4205b71e6fc8c0e51afcbe878fe33
srdayama <44225651+srdayama@users.noreply.github.com>
2018-11-13 10:12:47 +0530

    Add files via upload

---
+++ ReportGenerationTest/src/main/java/com/scp/test/ATest.java
@@ -0,0 +1,33 @@
+package com.scp.test;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(value=TestListner.class)
+public class ATest {
+
+	@Test
+	public void m1(){
+		System.out.println("m1");
+	}
+	
+	@Test
+	public void m2(){
+		System.out.println("m2");
+	}
+	
+	@Test
+	public void m3(){
+		System.out.println("m3");
+	}
+	
+	@Test
+	public void m4(){
+		System.out.println("m4");
+	}
+	
+	@Test
+	public void m5(){
+		System.out.println("m5");
+	}
+}
